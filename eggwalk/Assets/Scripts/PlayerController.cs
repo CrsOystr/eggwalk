7208b22bb0384edb23d993eec0b800e2175f8f9d
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-10-21 08:32:46 -0600

    Added Player Movement
    - currently uses WASD
    - PlayerController.cs handles input from the controller and sends it to:
    - PlayerMotor.cs, which does all the movement and adding force to the
    player's rigidbody
    - Adjust movement speed from the inspector to whatever feels right
    - Player can jump infinitely, need to fix that

---
+++ eggwalk/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This class takes in input and sends signals to the PlayerMotor.
+ * 
+ * PlayerController = handles input
+ * PlayerMotor = actually moves the player
+ */
+
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+
+	private PlayerMotor motor;
+
+	void Start () {
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update () {
+		// initilize inputs to zero
+		float horzInput = 0;
+		float vertInput = 0;
+
+		// input for keyboard control
+		// button input can be -1, 0, or 1
+		if (Input.GetButton ("W"))
+			vertInput = 1.0f;
+		if (Input.GetButton ("S"))
+			vertInput = -1.0f;
+		if (Input.GetButton ("D"))
+			horzInput = 1.0f;
+		if (Input.GetButton ("A"))
+			horzInput = -1.0f;
+
+		motor.setXZvelocity (horzInput, vertInput);
+
+		// jump!
+		if (Input.GetButton ("Jump"))
+			motor.addJumpForce();
+
+	}
+}
---
+++ eggwalk/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This moves the player (by adding forces and velocities to the player's rigidbody) based on signals from the PlayerController.
+ * 
+ * PlayerController = handles input
+ * PlayerMotor = actually moves the player
+ */
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField] public float forwardSpeed; // the player's run speed (Z direction)
+	[SerializeField] public float strafeSpeed; // the player's strafe speed (X direction)
+	[SerializeField] public float jumpForce; // the force applied to the rigid body when the jump button is pressed
+
+	private float xVel, zVel; // x = side to side, z = forward and back
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// FixedUpdate e is used because we are doing things with physics.
+	void FixedUpdate () {
+		Vector3 currentVelocity = rigidBody.velocity;
+		Vector3 newVelocity = new Vector3(
+			xVel * Time.deltaTime,
+			currentVelocity.y, // keep the rigidbody's y velocity
+			zVel * Time.deltaTime);
+		rigidBody.velocity = newVelocity;
+	}
+
+	public void addJumpForce() {
+		// and jump force in the world's up direction
+		rigidBody.AddForce (Vector3.up * jumpForce, ForceMode.Impulse); // ForceMode.Impulse creates more of a "pop" when the force is applied
+	}
+
+	public void setXZvelocity(float xInput, float zInput) {
+		xVel = Mathf.Lerp(xVel, strafeSpeed * xInput, 0.1f); // use Mathf.Lerp to smooth values
+		zVel = Mathf.Lerp(zVel, forwardSpeed, 0.5f);
+	}
+}
