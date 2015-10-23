f0bba24cad98922457f5f4f9f3ef03fc1fb775cc
Nathaniel Ellingson <nathaniel@Administrators-MacBook-Pro-2.local>
2015-10-23 10:57:08 -0600

    Added "Detect Player Trigger" object
    - Added relevant DetectPlayerTriggerBehavior.cs script
    - Has a SwitchBehavior component that turns on only when an object with
    the tag “Player” enters
    - Also gave the Player object the “Player” tag

---
+++ eggwalk/Assets/Scripts/DetectPlayerTriggerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SwitchBehavior))]
+[RequireComponent(typeof(BoxCollider))]
+
+public class DetectPlayerTriggerBehavior : MonoBehaviour {
+
+	private SwitchBehavior sb;
+
+	void Start() {
+		sb = GetComponent<SwitchBehavior> ();
+	}
+
+	void OnTriggerEnter(Collider col) {
+
+		if (col.gameObject.tag == "Player")
+			sb.setOn (true);
+	}
+	
+	void OnTriggerExit(Collider col) {
+		
+		if (col.gameObject.tag == "Player")
+			sb.setOn (false);
+		
+	}
+
+}
