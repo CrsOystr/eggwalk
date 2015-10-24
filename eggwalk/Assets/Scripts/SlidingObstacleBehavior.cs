23843d973ffdaab06eca16e069aba4b22102a24f
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-10-24 10:42:43 -0600

    Added Sliding Obstacle
    - Let me know what you guys think
    - Sliding objects have a child "Detect Player Trigger" object. When the
    player walks through the trigger, the object moves to a specified
    location.
    - I'm using Vector3.Lerp to move the object. I think there's probably a
    better way to go about this, but it works for now
    - Allowed the player's RigidBody to rotate on the Z axis. Strafing and
    running into objects now tips the player without any new code.
    - Weird errors in Unity "GameObject references runtime script in scene
    file. Fixing!" NO IDEA what this means.
    - There were weird errors when I first loaded the project, it said the
    HUD objects were missing scripts... don't know why...

---
+++ eggwalk/Assets/Scripts/SlidingObstacleBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Triggerable))]
+
+public class SlidingObstacleBehavior : MonoBehaviour {
+
+	public float moveLerp; // how fast the object slides to the target position;
+	public Vector3 targetOffset; // the position (relative the this object's initial position!) the object should slide to
+
+	private GameObject player;
+	private Triggerable triggerable;
+	private Rigidbody rigidBody;
+	private bool targetSet;
+	private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		triggerable = GetComponent<Triggerable> ();
+		rigidBody = GetComponent<Rigidbody> ();
+		targetSet = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (triggerable.isTriggered () && !targetSet) {
+			target = transform.position + targetOffset;
+			targetSet = true;
+		}
+
+		if (targetSet) {
+			Vector3 newPos = Vector3.Lerp(transform.position, target, moveLerp);
+			rigidBody.MovePosition(newPos);
+		}
+	}
+}
