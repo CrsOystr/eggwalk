efe395e6963ed8367493281c5dd2d53383be5acc
giosoto <giovannisoto@live.com>
2015-10-29 14:13:09 -0600

    Added Stacey's UI Bits

--- eggwalk/Assets/Scripts/Gameplay/GameplayObserver.cs
+++ eggwalk/Assets/Scripts/Gameplay/GameplayObserver.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameplayObserver : MonoBehaviour, Observer {
+public class GameplayObserver : MonoBehaviour, Observer
+{
 
-	public void onNotify(GameEvent e) {
-		GameEnumerations.EventCategory category = e.Category;
-		PlayerCharacter playerCharacter = e.Entity.GetComponent<PlayerCharacter> ();
-		switch (category) {
-			case GameEnumerations.EventCategory.Player_IsHurt:
-			{
-				playerCharacter.RecieveDamage(1);
-				break;
-			}
-			case GameEnumerations.EventCategory.Player_IsDead:
-			{
-				break;
-			}
+    public void onNotify(GameEvent e)
+    {
+        GameEnumerations.EventCategory category = e.Category;
+        switch (category)
+        {
+            case GameEnumerations.EventCategory.Player_IsHurt:
+                {
+                    PlayerCharacter playerCharacter = e.Entity.GetComponent<PlayerCharacter>();
+                    playerCharacter.RecieveDamage(1);
+                    break;
+                }
+            case GameEnumerations.EventCategory.Player_IsDead:
+                {
+                    break;
+                }
 
         }
-	}
+    }
 }
--- eggwalk/Assets/Scripts/UI/UIObserver.cs
+++ eggwalk/Assets/Scripts/UI/UIObserver.cs
@@ -1,38 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UIObserver : MonoBehaviour, Observer {
-	[SerializeField] private UISystem UISys;
+public class UIObserver : MonoBehaviour, Observer
+{
+    [SerializeField]
+    private UISystem UISys;
 
-	public void onNotify(GameEvent e) {
-		GameEnumerations.EventCategory category = e.Category;
-		PlayerCharacter playerCharacter = e.Entity.GetComponent<PlayerCharacter>();
-		
-		switch (category) {
-			case GameEnumerations.EventCategory.Player_IsHurt:
-			{
-				int currentLives = playerCharacter.P_CurrentLives;
-				int totalLives = playerCharacter.P_TotalLives;
+    public void onNotify(GameEvent e)
+    {
+        GameEnumerations.EventCategory category = e.Category;
 
-				if (currentLives < 0) {
-					UISys.setCurrentLives(0, totalLives);
-				} else {
-					UISys.setCurrentLives(currentLives, totalLives);
-				}
+        switch (category)
+        {
+            case GameEnumerations.EventCategory.Player_IsHurt:
+                {
+                    PlayerCharacter playerCharacter = e.Entity.GetComponent<PlayerCharacter>();
+                    int currentLives = playerCharacter.P_CurrentLives;
+                    int totalLives = playerCharacter.P_TotalLives;
 
-				break;
-			}
-			case GameEnumerations.EventCategory.Player_IsDead:
-			{
-				UISys.setVisibilityToRestart(true);
-				break;
-			}
+                    if (currentLives < 0)
+                    {
+                        UISys.setCurrentLives(0, totalLives);
+                    }
+                    else
+                    {
+                        UISys.setCurrentLives(currentLives, totalLives);
+                    }
+
+                    break;
+                }
+            case GameEnumerations.EventCategory.Player_IsDead:
+                {
+                    UISys.setVisibilityToRestart(true);
+                    break;
+                }
             case GameEnumerations.EventCategory.Player_HasRotatedHands:
-            {
-                float rotation = playerCharacter.P_RollingRotation;
-                UISys.setRotationToBalanceBar(rotation);
-                break;
-            }
+                {
+                    PlayerCharacter playerCharacter = e.Entity.GetComponent<PlayerCharacter>();
+                    float rotation = playerCharacter.P_RollingRotation;
+                    UISys.setRotationToBalanceBar(rotation);
+                    break;
+                }
         }
-	}
+    }
 }
