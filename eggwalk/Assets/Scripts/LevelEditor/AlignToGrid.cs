d2793cca0f5cc45885198bdd9aff4c3034bf208f
Nathaniel Ellingson <nathaniel@Administrators-MacBook-Pro-2.local>
2015-11-06 10:52:05 -0700

    Created AlignToGrid.cs
    - Aligns objects to a grid while in the Unity editor (so it technically
    runs in “play” mode but shouldn’t run in a build)
    - Size of grid can be specified by the user
    - Toggle snap to X/Y/Z, probably want to set Y to false.

---
+++ eggwalk/Assets/Scripts/LevelEditor/AlignToGrid.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class AlignToGrid : MonoBehaviour {
+	
+	public float gridSize;
+
+	public bool alignToX, alignToY, alignToZ;
+	void Update() {
+
+		if(!EditorApplication.isPlaying) return;
+
+		float newX, newY, newZ;
+		
+		if (alignToX)
+			newX = roundToGridSize (transform.position.x);
+		else
+			newX = transform.position.x;
+		
+		if (alignToY)
+			newY = roundToGridSize (transform.position.y);
+		else
+			newY = transform.position.y;
+		
+		if (alignToZ)
+			newZ = roundToGridSize (transform.position.z);
+		else
+			newZ = transform.position.z;
+
+		Vector3 newPos = new Vector3 (newX, newY, newZ);
+
+		transform.position = newPos;
+
+		Debug.Log ("test");
+
+	}
+
+	private float roundToGridSize(float r) {
+		return r - (r % gridSize);
+	}
+
+}
