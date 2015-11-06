37f1144eb2b89c23207bcdc87c34ce782e0931b2
Nathaniel Ellingson <nathaniel@Administrators-MacBook-Pro-2.local>
2015-11-06 11:28:30 -0700

    Added SnapToFloor.cs
    - set threshold variable (how high above the floor can it be and still
    snap)
    - “active” variable allows user to toggle snapping behavior

---
+++ eggwalk/Assets/Scripts/LevelEditor/SnapToFloor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapToFloor : MonoBehaviour {
+
+	public float threshold;
+
+	public bool active; // turn on/off snapping behavior
+
+	void Update () {
+#if UNITY_EDITOR
+
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position, -Vector3.up, out hit) && active) {
+			if(hit.distance <= threshold) {
+				Vector3 newPos = new Vector3 (
+					transform.position.x,
+					transform.position.y - hit.distance,
+					transform.position.z);
+
+				transform.position = newPos;
+			}
+		}
+
+#endif
+	}
+}
