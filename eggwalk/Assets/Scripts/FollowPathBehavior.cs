b87c58443e422898cf425efc01c990984633cde1
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-11-10 21:55:31 -0700

    Added "FollowPathBehavior.cs"
    - uses an array of Transforms to define a path for the object to follow
    - will be used for vehicles for the Sprint 2 build
    - eventually I would like to replace this with an actual driving AI
    (tryna get a job at Google SDC)

---
+++ eggwalk/Assets/Scripts/FollowPathBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class FollowPathBehavior : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public Transform[] pathPoints; // the array of points the object move towards
+	// !!!!! these points MUST NOT be children of this object, they must exist independent of this object !!!!!!
+
+	public bool stopped;
+
+	private int pathIndex;
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		pathIndex = 0;
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stopped || pathPoints.Length == 0)
+			return;
+
+		transform.LookAt (pathPoints[pathIndex]);
+
+		Vector3 newPos = Vector3.MoveTowards (transform.position, pathPoints [pathIndex].position, moveSpeed * Time.deltaTime);
+
+		if (newPos == pathPoints [pathIndex].position) {
+			pathIndex++;
+			if(pathIndex > pathPoints.Length - 1) 
+				pathIndex = 0;
+		}
+		transform.position = newPos;
+	}
+}
