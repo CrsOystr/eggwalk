908c946d5ee097350a8c038e9f4d4c69e16f6fab
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-11-12 15:50:33 -0700

    Created "City_Master"
    - 3x3 grid of streets
    - added a "Test Car" to show how vehicles will work
    - player systems still need to be set up correctly
    - sidewalks, buildings, and basically every prop still needs to be added

--- eggwalk/Assets/Scripts/FollowPathBehavior.cs
+++ eggwalk/Assets/Scripts/FollowPathBehavior.cs
@@ -19,6 +19,9 @@ public class FollowPathBehavior : MonoBehaviour {
 	void Start () {
 		pathIndex = 0;
 		rigidbody = GetComponent<Rigidbody> ();
+
+		removeY (); // set all y values in pathPoints to be equal to this object's y (paths are only 2D)
+		transform.LookAt (pathPoints[pathIndex]);
 	}
 	
 	// Update is called once per frame
@@ -26,7 +29,6 @@ public class FollowPathBehavior : MonoBehaviour {
 		if (stopped || pathPoints.Length == 0)
 			return;
 
-		transform.LookAt (pathPoints[pathIndex]);
 
 		Vector3 newPos = Vector3.MoveTowards (transform.position, pathPoints [pathIndex].position, moveSpeed * Time.deltaTime);
 
@@ -34,7 +36,20 @@ public class FollowPathBehavior : MonoBehaviour {
 			pathIndex++;
 			if(pathIndex > pathPoints.Length - 1) 
 				pathIndex = 0;
+
+			transform.LookAt (pathPoints[pathIndex]);
 		}
 		transform.position = newPos;
+	}
+
+	private void removeY() {
+		for (int i = 0; i < pathPoints.Length; i++) {
+			Vector3 newPos = new Vector3 (
+				pathPoints[i].position.x,
+				transform.position.y,
+				pathPoints[i].position.z);
+
+			pathPoints[i].position = newPos;
+		}
 	}
 }
