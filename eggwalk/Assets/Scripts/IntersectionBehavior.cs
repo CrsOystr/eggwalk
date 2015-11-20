0298404953d6c535e4fa11298be8434b39af7fe6
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-11-19 18:39:45 -0700

    Continued work on VehicleBehavior.cs
    - working in City_Test_Nathaniel.unity
    - added Intersection prefab, has the IntersectionBehavior.cs script
    - for whatever reason, when I add force a vehicle, it turns slightly. I
    have NO IDEA what causes this, very frustrating

---
+++ eggwalk/Assets/Scripts/IntersectionBehavior.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntersectionBehavior : MonoBehaviour {
+
+	public bool northClosed, southClosed, eastClosed, westClosed;
+
+}
---
+++ eggwalk/Assets/Scripts/VehicleBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class VehicleBehavior : MonoBehaviour {
+
+	public float driveSpeed; // target drive speed
+	public float acceleration; // force applied to accelerate vehicle
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// accelerate to desired speed
+		if (rb.velocity.magnitude < driveSpeed) {
+			rb.AddForce(transform.forward * acceleration, ForceMode.Force);
+		}
+
+	}
+}
