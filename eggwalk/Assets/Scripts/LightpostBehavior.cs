9120703e15f91869d38de75bdd13949083791294
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-11-21 15:17:23 -0700

    Imported Buildings and props

---
+++ eggwalk/Assets/Scripts/LightpostBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightpostBehavior : MonoBehaviour {
+
+	public Light spotlight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
