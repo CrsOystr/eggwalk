37ec3a8cfbeaebf5daa660c1d2319d77a60ae0d4
CrsOystr <nicmetz@jackmoves.com>
2015-11-26 18:05:34 -0700

    Added scripts for the soundsystem and modified the soundobserver for a simple example. Changed the main scene to have a simple honk sound when a player is injured, just as an example to show keith.

--- eggwalk/Assets/Scripts/Sound/SoundObserver.cs
+++ eggwalk/Assets/Scripts/Sound/SoundObserver.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class SoundObserver : MonoBehaviour, Observer {
 
+	[SerializeField] private SoundSystem SoundSys;
+
 	public void onNotify(GameEvent e) {
 		GameEnumerations.EventCategory category = e.Category;
 		
 		switch (category) {
 			case GameEnumerations.EventCategory.Player_IsHurt:
 			{
+				SoundSys.playExampleAudio();
 				break;
 			}
 			case GameEnumerations.EventCategory.Player_IsDead:
---
+++ eggwalk/Assets/Scripts/Sound/SoundSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundSystem : MonoBehaviour {
+	
+	/* 	ADD REFERENCES TO NEW SOUNDS YOU WILL WANT TO USE HERE:
+	*	use the format below but use a descriptive name in palace of "exampleAudio"
+	*	you will then have to drag the actual audio source from the object it is on to this script.
+	*	This script can be found under GameplaySystem/PlayerNotifier/SoundObserver in the scene.
+	*	Contact Nic with questions/concerns
+	 */
+
+	[SerializeField] private AudioSource exampleAudio;
+
+	//example of how to do multiple items I.E. all firetrucks
+	[SerializeField] private AudioSource[] exampleAudioList;
+
+
+
+	/*FUNCTIONS TO USE THE ABOVE AUDIOSOURCE:
+	 * use descriptive function names, these will be accessesed through the SOUNDOBSERVER script.
+	 * Seperate functions out as much as possible, I.E. make a "turn on all firetrucks" and "turn on waterfountain sound"
+	 * as apposed to "put out the fire sounds"
+	 */
+	public void playExampleAudio()
+	{
+		exampleAudio.Play ();
+		//exampleAudio.enabled (false);
+		//exampleAudio.Stop ();
+	}
+
+	public void playExampleAudioList()
+	{
+		foreach (AudioSource source in exampleAudioList)
+			exampleAudio.Play ();
+		//exampleAudio.enabled (false);
+		//exampleAudio.Stop ();
+	}
+
+}
