93be2c9e4d9e36e4fe7cb2377d6d906cd6c7dd26
Nathaniel Ellingson <nathaniel@Administrators-MacBook-Pro.local>
2016-01-26 10:25:12 -0700

    All PhysicsOnCollision objects can now be made to reset
    — only applied to City_Master.unity
    — specify secondsToReset variable from inspector
    — will only reset when outside of the camera’s frustrum
    — I set lampposts to 60 seconds and people to 10 seconds

--- eggwalk/Assets/Scripts/PhysicsOnCollision.cs
+++ eggwalk/Assets/Scripts/PhysicsOnCollision.cs
@@ -5,11 +5,46 @@ using System.Collections;
 
 public class PhysicsOnCollision : MonoBehaviour {
 
-	private Rigidbody rb;
+	[SerializeField] private float _secondsToReset;
+
+	private Vector3 _initPosition;
+	private Quaternion _initRotation;
+	private float _resetCounter;
+	private Rigidbody _rb;
 
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
-		rb.isKinematic = true;
+		_rb = GetComponent<Rigidbody> ();
+		_rb.isKinematic = true;
+		
+		_initPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		_initRotation = Quaternion.Euler(transform.rotation.eulerAngles);
+	}
+	
+	void Update() {
+		if(_secondsToReset > 0) {
+		
+			_resetCounter += Time.deltaTime;
+			if(_resetCounter >= _secondsToReset) {
+			
+				if(!isInsideCameraFrustrum()) {
+					transform.position = _initPosition;
+					transform.rotation = _initRotation;
+					Debug.Log ("succesful reset!");
+				} else {
+					_resetCounter = 0;
+					//Debug.Log ("it was on screen...");
+				}
+			
+			}
+		
+		}
+	}
+	
+	private bool isInsideCameraFrustrum() {
+	
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+		return GeometryUtility.TestPlanesAABB(planes, GetComponent<Collider>().bounds);
+		
 	}
 
 	void OnCollisionEnter(Collision col) {
@@ -17,7 +52,7 @@ public class PhysicsOnCollision : MonoBehaviour {
 		Rigidbody colRB = col.gameObject.GetComponent<Rigidbody> ();
 
 		if (colRB != null)
-			rb.isKinematic = false;
+			_rb.isKinematic = false;
 
 	}
 
